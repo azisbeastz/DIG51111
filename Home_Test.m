bed2150b956c033e939d03b4d2e194a61b898582
Arun Hashmi <s22136249@bcud2-024468.students.uce.ac.uk>
2025-01-29 14:23:47 +0000

    rice and doughnuts

---
+++ Home_Test.m
@@ -0,0 +1,17 @@
+% Assign the expression to the variable A
+A = 3 + 4/5 + 6;
+
+% Print out the value of A
+disp(A);
+
+% Task 2
+B = 2*pi^2 
+disp(B);
+
+%Task 3
+C = sqrt(2)
+disp(C);
+
+%Task 4
+D = (0.0000123 + 5.67*10^-3)*(0.4567*10^-4)
+disp(D);
