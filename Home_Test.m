0d2a3a38a6378c4cd5cced0ea8e14537281629fa
Arun Hashmi <s22136249@bcud2-024468.students.uce.ac.uk>
2025-01-29 14:36:02 +0000

    Resolves merge beans

--- Home_Test.m
+++ Home_Test.m
@@ -1,17 +1,18 @@
+% Task 1
 % Assign the expression to the variable A
-A = 3 + 4/5 + 6;
-
-% Print out the value of A
-disp(A);
+A = (3+4)/(5+6);
+% Print out the value of A with a custom message
+disp(['A = ', num2str(A)]);
 
 % Task 2
-B = 2*pi^2 
-disp(B);
+B = 2*pi^2 ;
+disp(['B = ', num2str(B)]);
 
-%Task 3
-C = sqrt(2)
-disp(C);
+% Task 3
+C = sqrt(2);
+disp(['C = ', num2str(C)]);
 
-%Task 4
-D = (0.0000123 + 5.67*10^-3)*(0.4567*10^-4)
-disp(D);
+% Task 4
+D = (0.0000123 + 5.67*10^-3)*(0.4567*10^-4);
+disp(['D = ', num2str(D)]);
+
